550ae92370bbc2dc0d7938b5d8fcbd35147e4c52
monwar23 <monwar2345@gmail.com>
2024-05-01 02:32:15 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import About from "../Components/About";
+import Banner from "../Components/Banner";
+import Services from "../Components/Services";
+
+const Home = () => {
+    return (
+        <div>
+           <Banner></Banner>
+           <About></About>
+           <Services></Services>
+        </div>
+    );
+};
+
+export default Home;
