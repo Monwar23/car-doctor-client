fd5e98abf0aaed75715618fb8e0d1ef33362fe1f
monwar23 <monwar2345@gmail.com>
2024-05-03 19:09:07 +0600

    add location in private routes

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,15 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
 
-    const {user, loading} = useContext(AuthContext);
-    if(loading) {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation()
+
+    
+    if (loading) {
         return <progress className="progress w-56"></progress>
     }
 
-    if(user?.email){
+    if (user?.email) {
         return children;
     }
 
